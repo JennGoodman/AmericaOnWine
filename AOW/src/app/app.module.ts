b2e5e6038b381c1cc58a3e8f3080c4fa688519a5
maxstackoverflow <maxstackoverflow@outlook.com>
2018-02-08 23:47:24 -0500

    Added bootstrap to dependencies, added it to global styles, and began work on inventory form component

--- AOW/src/app/app.module.ts
+++ AOW/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { InventoryFormComponent } from './inventory-form/inventory-form.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InventoryFormComponent
   ],
   imports: [
     BrowserModule,
---
+++ AOW/src/app/inventory-form/inventory-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-inventory-form',
+  templateUrl: './inventory-form.component.html',
+  styleUrls: ['./inventory-form.component.css']
+})
+export class InventoryFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
