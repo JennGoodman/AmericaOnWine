c1bea2705f3e1e09ba85a9556313771665fac880
Jenn Goodman <russus@gmail.com>
2018-02-10 17:13:28 -0500

    Adding Data Models for future use

--- AOW/src/models/SubType.ts
+++ AOW/src/models/SubType.ts
@@ -1,9 +1,13 @@
-export class Type {
+import { Type } from './Type';
+
+export class SubType {
     id: number;
     type: string;
+    subType: Type;
 
     constructor() {
       this.id = null;
       this.type = null;
+      this.subType = null;
     }
   }
