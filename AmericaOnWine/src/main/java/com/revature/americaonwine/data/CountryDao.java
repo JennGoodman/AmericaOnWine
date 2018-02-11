350dc0c9fb67f508dd5b30a3b0196996905ebaea
Jenn Goodman <russus@gmail.com>
2018-02-11 18:58:53 -0500

    Completed Country Dao and Hibernate file

--- AmericaOnWine/src/main/java/com/revature/americaonwine/data/CountryDao.java
+++ AmericaOnWine/src/main/java/com/revature/americaonwine/data/CountryDao.java
@@ -1,6 +1,6 @@
 package com.revature.americaonwine.data;
 
-import java.util.ArrayList;
+import java.util.List;
 
 import com.revature.americaonwine.beans.Country;
 
@@ -8,27 +8,34 @@ public interface CountryDao {
 
 	/**
 	 * Add country to the database.
-	 * @param country String to be inserted into the table
+	 * 
+	 * @param country
+	 *            String to be inserted into the table
 	 * @return the id of the country added
 	 */
 	public int addCountry(String country);
-	
+
 	/**
-	 * Get set of all countries in the database
-	 * @return ArrayList<Country> of countries
+	 * Get list of all countries in the database
+	 * 
+	 * @return List<Country> of countries
 	 */
-	public ArrayList<Country> getAllCountries();
-	
+	public List<Country> getAllCountries();
+
 	/**
 	 * Updates an existing country in the db by ID.
-	 * @param id of the country to be updated
+	 * 
+	 * @param id
+	 *            of the country to be updated
 	 * @return true if successful, false otherwise
 	 */
 	public boolean updateCountry(int id);
-	
+
 	/**
 	 * Deletes an existing country in the db by ID.
-	 * @param id of the country to be deleted
+	 * 
+	 * @param id
+	 *            of the country to be deleted
 	 * @return true if successful, false otherwise
 	 */
 	public boolean deleteCountry(int id);
