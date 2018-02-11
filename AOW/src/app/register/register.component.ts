c0f3233b0ef791711d98612066a7064c522dd12a
Mickaele Perez <mick.p311@gmail.com>
2018-02-11 15:03:31 -0500

    creates user after they hit the register btn

--- AOW/src/app/register/register.component.ts
+++ AOW/src/app/register/register.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit, Input } from '@angular/core';
 
 import {NgForm} from '@angular/forms';  
 
-//import {User} from '../user'
+import {User} from '../../models/User'
+//import {User} from '../user';
 
 
 @Component({
@@ -13,12 +14,18 @@ import {NgForm} from '@angular/forms';
 
 export class RegisterComponent implements OnInit {
 //@Input() public user: User;
+@Input() public user: User =new User();
+id: number;
 email: string;
 username: string;
 password: string;
-accountType: string;
+accountType: String;
+active: number;
+cancceled: number;
 
-  constructor() { }
+
+  constructor() { 
+  }
 
   ngOnInit() {
   }
@@ -26,15 +33,23 @@ accountType: string;
   register(){
     if(this.email==null || this.username==null || this.password==null || this.accountType==null){
       alert("Need to fill out all the fields");
-    }else{
-      console.log(this.email);
-      console.log(this.username);
-      console.log(this.password);
-      console.log(this.accountType);
+    }
+    else{
+      this.user.id =1;
+      this.user.email=this.email;
+      this.user.username=this.username;
+      this.user.password=this.password;
+      if(this.accountType === 'customer'){
+        this.user.role = 1;
+      } 
+      else {
+        this.user.role = 2;
+      }
+      this.user.active=0;
+      this.user.cancelled=0;
+    }
+      console.log(this.user);
     }
     
   }
 
-  
-
-}
