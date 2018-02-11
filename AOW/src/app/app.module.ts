d2be8a372cc05b84f63f7734ee7e998faff81abc
Jenn Goodman <russus@gmail.com>
2018-02-10 20:34:57 -0500

    Adding Navbar and fixed an import in subtype.service.ts

--- AOW/src/app/app.module.ts
+++ AOW/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { InventoryFormComponent } from './inventory-form/inventory-form.componen
 import { RetailHomeComponent } from './retail-home/retail-home.component';
 
 import { FileUploadService } from '../services/file-upload.service';
+import { NavbarComponent } from './navbar/navbar.component';
 
 
 @NgModule({
@@ -16,7 +17,8 @@ import { FileUploadService } from '../services/file-upload.service';
     AppComponent,
     InventoryFormComponent,
       //CustomerRegisterAccountComponent,
-    RetailHomeComponent
+    RetailHomeComponent,
+      NavbarComponent
   ],
   imports: [
     BrowserModule,
