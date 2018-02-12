aa456c09d4f9e615a68fdcb82a6d858ae222dfb3
Jenn Goodman <russus@gmail.com>
2018-02-12 14:21:28 -0500

    Update aow.config.ts
    Updated Config ID

--- AOW/src/aow.config.ts
+++ AOW/src/aow.config.ts
@@ -5,7 +5,7 @@ export class Config {
     defaultHeaders: HttpHeaders;
 
     constructor() {
-        this.appURL = 'http://18.217.53.157:8080/americaonwine/';
+        this.appURL = 'http://18.219.46.59:8080/americaonwine/';
         this.defaultHeaders = new HttpHeaders();
         this.defaultHeaders.append('Content-Type', 'application/json');
         //this.defaultHeaders.append('enctype', 'multipart/form-data');
