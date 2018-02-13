e67c59eda2826acd61d1d18626d7c830be9ddcc9
Hasanain <hasanain.abdullah94@gmail.com>
2018-02-13 16:56:44 -0500

    logging errors in login

--- AmericaOnWine/src/main/java/com/revature/americaonwine/services/LoginHibernate.java
+++ AmericaOnWine/src/main/java/com/revature/americaonwine/services/LoginHibernate.java
@@ -1,5 +1,6 @@
 package com.revature.americaonwine.services;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -9,12 +10,15 @@ import com.revature.americaonwine.data.UserDao;
 @Component
 public class LoginHibernate implements LoginService {
 
+	private Logger log = Logger.getLogger(LoginHibernate.class);
 	@Autowired
 	private UserDao ud;
 	
 	@Override
 	public User login(String username, String password) {
+		log.trace("Attempting to login; Username: " + username + "; Password: " + password);
 		User u = ud.getUserByUsername(username);
+		log.trace("Recieved user " + u.toString());
 		if (u != null && u.getPassword().equals(password) )
 			return null;
 		else return u;
