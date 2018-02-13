e8c50ad17d2abe1442765b877d27f0d10da63edd
Hasanain <hasanain.abdullah94@gmail.com>
2018-02-13 11:52:30 -0500

    added cors for the login delegate

--- AmericaOnWine/src/main/java/com/revature/americaonwine/delegates/LoginController.java
+++ AmericaOnWine/src/main/java/com/revature/americaonwine/delegates/LoginController.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,7 +16,7 @@ import com.revature.americaonwine.beans.User;
 import com.revature.americaonwine.services.LoginService;
 
 @Controller
-// @CrossOrigin(origins="PUT ADDRESS HERE")
+@CrossOrigin(origins="http//localhost:8080")
 @RequestMapping(value="/login")
 public class LoginController {
 	
