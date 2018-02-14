3e5d6798bd76d771e9c7b159da8520e9bfea3611
Jenn Goodman <russus@gmail.com>
2018-02-14 02:12:09 -0500

    Possibly fixed Navbar?

--- AOW/src/app/navbar/navbar.component.ts
+++ AOW/src/app/navbar/navbar.component.ts
@@ -13,23 +13,23 @@ export class NavbarComponent implements OnInit {
   user: User;
 
   constructor(private service: AccountAccessService, private router: Router) {
-    window.addEventListener('click', this.loadUser);
-}
+    addEventListener('click', this.loadUser);
+  }
 
-loadUser() {
-  this.user = JSON.parse(localStorage.getItem('user'));
-  console.log('Constructor Ran: ' + this.user);
-}
+  loadUser() {
+    this.user = JSON.parse(localStorage.getItem('user'));
+    console.log('Constructor Ran: ' + this.user);
+  }
 
-ngOnInit() {
-}
+  ngOnInit() {
+  }
 
-logout() {
-  this.service.logout().subscribe(resp => {
-    resp = resp as string;
-    localStorage.removeItem('user');
-    this.user = null;
-    this.router.navigate(['']);
-  });
-}
+  logout() {
+    this.service.logout().subscribe(resp => {
+      const result = resp as string;
+      localStorage.removeItem('user');
+      this.user = null;
+      this.router.navigate(['']);
+    });
+  }
 }
