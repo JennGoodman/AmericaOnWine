e2010264d96d4591ddaa390ce78687145f001596
maxstackoverflow <maxstackoverflow@outlook.com>
2018-02-13 21:37:10 -0500

    adding component annotation to services

--- AmericaOnWine/src/main/java/com/revature/americaonwine/services/InventoryService.java
+++ AmericaOnWine/src/main/java/com/revature/americaonwine/services/InventoryService.java
@@ -2,8 +2,11 @@ package com.revature.americaonwine.services;
 
 import java.util.List;
 
+import org.springframework.stereotype.Component;
+
 import com.revature.americaonwine.beans.InventoryItem;
 
+@Component
 public interface InventoryService {
 	public List<InventoryItem> getAll();
 	public InventoryItem add(InventoryItem i);
