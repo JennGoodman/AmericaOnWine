5b6ba1a104f3e04c59617bef03418b04eb9bed53
Matthew Little <mlittl9199@gmail.com>
2018-02-13 21:44:13 -0500

    fixing autowiring bugs in wineformhibernate and inventory controller

--- AmericaOnWine/src/main/java/com/revature/americaonwine/services/InventoryService.java
+++ AmericaOnWine/src/main/java/com/revature/americaonwine/services/InventoryService.java
@@ -2,11 +2,8 @@ package com.revature.americaonwine.services;
 
 import java.util.List;
 
-import org.springframework.stereotype.Component;
-
 import com.revature.americaonwine.beans.InventoryItem;
 
-@Component
 public interface InventoryService {
 	public List<InventoryItem> getAll();
 	public InventoryItem add(InventoryItem i);
