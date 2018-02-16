a713074b5a108a23a2e65ae3458470ce151179d5
Jenn Goodman <russus@gmail.com>
2018-02-16 14:13:32 -0500

    Initial Branch Push. Empty 'search' component added.

--- AOW/src/app/app.module.ts
+++ AOW/src/app/app.module.ts
@@ -29,6 +29,7 @@ import { TagService } from '../services/tag.service';
 import { TransactionService } from '../services/transaction.service';
 import { TypeService } from '../services/type.service';
 import { HomeComponent } from './home/home.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
@@ -43,7 +44,8 @@ import { HomeComponent } from './home/home.component';
     WineItemComponent,
     WineListComponent,
     BigWineItemComponent,
-    HomeComponent
+    HomeComponent,
+    SearchComponent
   ],
   imports: [
     HttpClientModule,
