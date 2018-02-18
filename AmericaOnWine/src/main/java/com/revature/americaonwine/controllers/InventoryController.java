ed4dbb79be13c8aafe5d1ba51983eb2a0e3d8fc0
Hasanain <hasanain.abdullah94@gmail.com>
2018-02-17 20:58:02 -0500

    users that are not logged in can view items

--- AmericaOnWine/src/main/java/com/revature/americaonwine/controllers/InventoryController.java
+++ AmericaOnWine/src/main/java/com/revature/americaonwine/controllers/InventoryController.java
@@ -40,6 +40,10 @@ public class InventoryController {
 	@RequestMapping(method=RequestMethod.GET, produces={"application/json; charset=UTF-8"})
 	public String getAll(HttpSession ses, ObjectMapper om) throws JsonProcessingException {
 		User u = (User) ses.getAttribute("user");
+		if (u == null) {
+			u = new User();
+			u.setRole(2);
+		}
 		return om.writeValueAsString(is.getForUser(u));
 	}
 	
