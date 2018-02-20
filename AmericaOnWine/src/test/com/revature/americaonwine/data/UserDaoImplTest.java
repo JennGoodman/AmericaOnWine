f472c3a61084f49cafc1bc3106b0d480294157bb
Matthew Little <mlittl9199@gmail.com>
2018-02-20 14:34:50 -0500

    forgot a file

--- AmericaOnWine/src/test/com/revature/americaonwine/data/UserDaoImplTest.java
+++ AmericaOnWine/src/test/com/revature/americaonwine/data/UserDaoImplTest.java
@@ -1,6 +1,9 @@
 package com.revature.americaonwine.data;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.util.Random;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -8,9 +11,15 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import com.revature.americaonwine.beans.Roles;
+import com.revature.americaonwine.beans.User;
+
 public class UserDaoImplTest {
 	
 	static UserDao ud;
+	static User c1;
+	static User r2;
+	static User a3;
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -22,7 +31,14 @@ public class UserDaoImplTest {
 
 	@Before
 	public void setUp() throws Exception {
+		Random r = new Random();
 		ud = new UserHibernate();
+		long l = r.nextLong();
+		c1 = new User(100000, "testCustomer" + l, "", "testCustEmail" + l, Roles.numericalRepresentation(Roles.CUSTOMER), 1, 0);
+		l = r.nextLong();
+		r2 = new User(100001, "testRetailer" + l, "", "testRetailEmail" + l, Roles.numericalRepresentation(Roles.RETAILER), 1, 0);
+		l = r.nextLong();
+		a3 = new User(100002, "testAdmin" + l, "", "testAdminEmail" + l, Roles.numericalRepresentation(Roles.ADMIN), 1, 0);
 	}
 
 	@After
@@ -31,17 +47,44 @@ public class UserDaoImplTest {
 
 	@Test
 	public void testInsertUser() {
-		assert(true, ud.insertUser());
+		assertEquals(true, ud.insertUser(c1));
+		User u = ud.getUserByUsername(c1.getUsername());
+		
+		assertEquals(c1.getUsername(), u.getUsername());
+		assertEquals(c1.getPassword(), u.getPassword());
+		assertEquals(c1.getEmail(), u.getEmail());
+		assertEquals(c1.getRole(), u.getRole());
+		assertEquals(c1.getActive(), u.getActive());
+		assertEquals(c1.getCancelled(), u.getCancelled());
+		
+		assertEquals(false, ud.insertUser(c1));
 	}
 
 	@Test
 	public void testGetUser() {
-		fail("Not yet implemented");
+		ud.insertUser(c1);
+		User ubyUser = ud.getUserByUsername(c1.getUsername());
+		User u = ud.getUser(ubyUser.getId());
+		
+		assertEquals(c1.getUsername(), u.getUsername());
+		assertEquals(c1.getPassword(), u.getPassword());
+		assertEquals(c1.getEmail(), u.getEmail());
+		assertEquals(c1.getRole(), u.getRole());
+		assertEquals(c1.getActive(), u.getActive());
+		assertEquals(c1.getCancelled(), u.getCancelled());
 	}
 
 	@Test
 	public void testGetUserByUsername() {
-		fail("Not yet implemented");
+		ud.insertUser(c1);
+		User u = ud.getUserByUsername(c1.getUsername());
+		
+		assertEquals(c1.getUsername(), u.getUsername());
+		assertEquals(c1.getPassword(), u.getPassword());
+		assertEquals(c1.getEmail(), u.getEmail());
+		assertEquals(c1.getRole(), u.getRole());
+		assertEquals(c1.getActive(), u.getActive());
+		assertEquals(c1.getCancelled(), u.getCancelled());
 	}
 
 	@Test
