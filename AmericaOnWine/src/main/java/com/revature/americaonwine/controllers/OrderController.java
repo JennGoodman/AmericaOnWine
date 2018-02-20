eaee9ae286114a46e09630f14b02c60056047ccf
Hasanain <hasanain.abdullah94@gmail.com>
2018-02-20 12:40:20 -0500

    using restcontroller

--- AmericaOnWine/src/main/java/com/revature/americaonwine/controllers/OrderController.java
+++ AmericaOnWine/src/main/java/com/revature/americaonwine/controllers/OrderController.java
@@ -1,5 +1,7 @@
 package com.revature.americaonwine.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.revature.americaonwine.beans.InventoryItem;
+import com.revature.americaonwine.beans.Transaction;
 import com.revature.americaonwine.services.TransactionService;
 
 @RestController
@@ -18,13 +21,14 @@ public class OrderController {
 	private TransactionService ts;
 	
 	@RequestMapping(method=RequestMethod.GET, produces={"application/json; charset=UTF-8"})
-	public String maxOrderNumber() throws JsonProcessingException {
+	public String maxOrderNumber() throws JsonProcessingException 
+	{
 		return Integer.toString(ts.getMax());
 	}
 	
 	@RequestMapping(method=RequestMethod.POST, produces= {"application/json; charset=UTF-8"})
-	public String getItemsByInv(@RequestBody InventoryItem inv, ObjectMapper om) throws JsonProcessingException 
+	public List<Transaction> getItemsByInv(@RequestBody InventoryItem inv, ObjectMapper om) throws JsonProcessingException 
 	{
-		return om.writeValueAsString(ts.getTransByInv(inv));
+		return ts.getTransByInv(inv);
 	}
 }
